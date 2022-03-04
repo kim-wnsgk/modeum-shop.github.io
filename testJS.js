57556fbabf7cd5c729f78b8e3ff39dbf910cf710
Kim Junha <kim_jh6859@naver.com>
2022-03-05 01:04:49 +0900

    edit index.html, add script

--- testJS.js
+++ testJS.js
@@ -1,1 +1,12 @@
-let grades
+const leftImage = document.querySelector(".leftImage");
+const rightImage = document.querySelector(".rightImage");
+const image = document.querySelector(".image");
+
+
+leftImage.addEventListener("click", function(){
+    image.style.backgroundImage = "url(./images/sleeping_mozzi.jpg)";
+})
+rightImage.addEventListener("click", function(){
+    image.style.backgroundImage = "url(./images/deumseong.jpg)";
+})
+
